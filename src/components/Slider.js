a405668820ad4a6e78968ddaab02725d750af9a0
Kaylin Lapan <kaylinlapan@gmail.com>
2022-06-07 19:57:38 -0400

    Starting to implement state in slider component and styling so grid doesn't become column in mobile.

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,6 +1,10 @@
 import './Slider.scss';
+import React, { useState } from 'react';
+
 
 function SliderContent(props){
+  const [thumbnail, setThumbnail] = useState('thumbnail1');
+  console.log(thumbnail);
   return (
     <div>
       <div className="row">
@@ -11,13 +15,15 @@ function SliderContent(props){
           <div className="row">
               <img src="https://picsum.photos/1325/1200" alt="test"/>
           </div>
+          <div class="container">
           <div className="row">
-            <div style={{backgroundImage: `url("${props.image1}")`}} id="thumbnail1" className="col-md-2 border thumbnailBox"></div>
-              <div style={{backgroundImage: `url("${props.image2}")`}} id="thumbnail2" className="col-md-2 border thumbnailBox"></div>
-              <div style={{backgroundImage: `url("${props.image3}")`}} id="thumbnail3" className="col-md-2 border thumbnailBox"></div>
-              <div style={{backgroundImage: `url("${props.image4}")`}} id="thumbnail4" className="col-md-2 border thumbnailBox"></div>
-              <div style={{backgroundImage: `url("${props.image5}")`}} id="thumbnail5" className="col-md-2 border thumbnailBox"></div>
-              <div style={{backgroundImage: `url("${props.image6}")`}} id="thumbnail6" className="col-md-2 border thumbnailBox"></div>
+            <div onClick={() => setThumbnail('thumbnail1')} style={{backgroundImage: `url("${props.image1}")`}} id="thumbnail1" className="col-2 border thumbnailBox"></div>
+              <div onClick={() => setThumbnail('thumbnail2')} style={{backgroundImage: `url("${props.image2}")`}} id="thumbnail2" className="col-2 border thumbnailBox"></div>
+              <div onClick={() => setThumbnail('thumbnail3')} style={{backgroundImage: `url("${props.image3}")`}} id="thumbnail3" className="col-2 border thumbnailBox"></div>
+              <div onClick={() => setThumbnail('thumbnail4')} style={{backgroundImage: `url("${props.image4}")`}} id="thumbnail4" className="col-2 border thumbnailBox"></div>
+              <div onClick={() => setThumbnail('thumbnail5')} style={{backgroundImage: `url("${props.image5}")`}} id="thumbnail5" className="col-2 border thumbnailBox"></div>
+              <div onClick={() => setThumbnail('thumbnail6')} style={{backgroundImage: `url("${props.image6}")`}} id="thumbnail6" className="col-2 border thumbnailBox"></div>
+          </div>
           </div>
         </div>
       </div>
