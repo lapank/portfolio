f9a4b4b646d1da7f76df427c9cf1754de76d4c7c
Kaylin Lapan <kaylinlapan@gmail.com>
2022-06-07 20:59:54 -0400

    State added for slider image, title, and text.

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -2,27 +2,31 @@ import './Slider.scss';
 import React, { useState } from 'react';
 
 
+
+
 function SliderContent(props){
-  const [thumbnail, setThumbnail] = useState('thumbnail1');
-  console.log(thumbnail);
+  const [thumbnail, setThumbnail] = useState(props.image1);
+  const [text, setText] = useState(props.text1);
+  const [title, setTitle] = useState(props.title1);
   return (
     <div>
       <div className="row">
         <div className="col-md-4 bg-light">
-          
+          <h3>{title}</h3>
+          <p>{text}</p>
         </div>
         <div className="col-md-8">
           <div className="row">
-              <img src="https://picsum.photos/1325/1200" alt="test"/>
+              <img src={thumbnail} alt="test"/>
           </div>
           <div class="container">
           <div className="row">
-            <div onClick={() => setThumbnail('thumbnail1')} style={{backgroundImage: `url("${props.image1}")`}} id="thumbnail1" className="col-2 border thumbnailBox"></div>
-              <div onClick={() => setThumbnail('thumbnail2')} style={{backgroundImage: `url("${props.image2}")`}} id="thumbnail2" className="col-2 border thumbnailBox"></div>
-              <div onClick={() => setThumbnail('thumbnail3')} style={{backgroundImage: `url("${props.image3}")`}} id="thumbnail3" className="col-2 border thumbnailBox"></div>
-              <div onClick={() => setThumbnail('thumbnail4')} style={{backgroundImage: `url("${props.image4}")`}} id="thumbnail4" className="col-2 border thumbnailBox"></div>
-              <div onClick={() => setThumbnail('thumbnail5')} style={{backgroundImage: `url("${props.image5}")`}} id="thumbnail5" className="col-2 border thumbnailBox"></div>
-              <div onClick={() => setThumbnail('thumbnail6')} style={{backgroundImage: `url("${props.image6}")`}} id="thumbnail6" className="col-2 border thumbnailBox"></div>
+            <div onClick={() => {setThumbnail(props.image1); setText(props.text1); setTitle(props.title1)}} style={{backgroundImage: `url("${props.image1}")`}} id="thumbnail1" className="col-2 border thumbnailBox"></div>
+              <div onClick={() => {setThumbnail(props.image2); setText(props.text2); setTitle(props.title2)}} style={{backgroundImage: `url("${props.image2}")`}} id="thumbnail2" className="col-2 border thumbnailBox"></div>
+              <div onClick={() => {setThumbnail(props.image3); setText(props.text3); setTitle(props.title3)}} style={{backgroundImage: `url("${props.image3}")`}} id="thumbnail3" className="col-2 border thumbnailBox"></div>
+              <div onClick={() => {setThumbnail(props.image4); setText(props.text4); setTitle(props.title4)}} style={{backgroundImage: `url("${props.image4}")`}} id="thumbnail4" className="col-2 border thumbnailBox"></div>
+              <div onClick={() => {setThumbnail(props.image5); setText(props.text5); setTitle(props.title5)}} style={{backgroundImage: `url("${props.image5}")`}} id="thumbnail5" className="col-2 border thumbnailBox"></div>
+              <div onClick={() => {setThumbnail(props.image6); setText(props.text6); setTitle(props.title6)}} style={{backgroundImage: `url("${props.image6}")`}} id="thumbnail6" className="col-2 border thumbnailBox"></div>
           </div>
           </div>
         </div>
@@ -35,15 +39,25 @@ function Slider(props){
     const item = props.item;
   const listItems = item.map((item) =>
     // Correct! Key should be specified inside the array.
-    <SliderContent key={item.title}
-      title = {item.title}
-      text = {item.text}
+    <SliderContent key={item.title1}
+      title1 = {item.title1}
       image1 = {item.image1}
-      image2 = {item.image2} 
-      image3 = {item.image3} 
-      image4 = {item.image4} 
-      image5 = {item.image5} 
-      image6 = {item.image6}  />
+      text1 = {item.text1}
+      title2 = {item.title2}
+      image2 = {item.image2}
+      text2 = {item.text2} 
+      title3 = {item.title3}
+      image3 = {item.image3}
+      text3 = {item.text3} 
+      title4 = {item.title4}
+      image4 = {item.image4}
+      text4 = {item.text4} 
+      title5 = {item.title5}
+      image5 = {item.image5}
+      text5 = {item.text5} 
+      title6 = {item.title6}
+      image6 = {item.image6}
+      text6 = {item.text6}  />
   );
   return (
     <div className="container pt-2">
--- src/components/SliderData.js
+++ src/components/SliderData.js
@@ -1,13 +1,22 @@
 export const sliderData = [
 	{
-		title: 'Art and Digital Media',
-		text: 'Drupal, Print & Web Design, Css3, HTML5, JavaScript6, Project Management, Public Engagment, Accessibility',
+		title1: 'Art and Digital Media',
 		image1: 'https://picsum.photos/325/325',
+		text1: 'Drupal, Print & Web Design, Css3, HTML5, JavaScript6, Project Management, Public Engagment, Accessibility',
+		title2: 'Wireframe',
 		image2: 'https://picsum.photos/680/625',
+		text2: 'Public Engagment, Accessibility',
+		title3: 'Mock-up',
 		image3: 'https://picsum.photos/407/325',
+		text3: 'Drupal, Print & Web Design, Css3, Public Engagment, Accessibility',
+		title4: 'Art and Digital Media',
 		image4: 'https://picsum.photos/400/225',
+		text4: 'Css3, HTML5, JavaScript6, Project Management, Public Engagment, Accessibility',
+		title5: 'Art and Digital Media',
 		image5: 'https://picsum.photos/300/325',
+		text5: 'Drupal, Print & Web Design, Css3, HTML5, JavaScript6',
+		title6: 'Wireframe',
 		image6: 'https://picsum.photos/440/325',
-		
+		text6: 'Drupal, Print & Web Design, Css3, HTML5, JavaScript6, Project Management, Public Engagment, Accessibility',
 	},
 ]
