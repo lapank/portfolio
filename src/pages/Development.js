ebecd1a6e022c31a74c2af58443bf5b3b5627e02
Kaylin Lapan <kaylinlapan@gmail.com>
2022-08-22 19:00:02 -0400

    Link styling and additional text to development page.

--- src/pages/Development.js
+++ src/pages/Development.js
@@ -25,14 +25,16 @@ function Development(){
 			<Slider item={sliderData}/>
 			<div className="container pt-5">
 			<h2 id="alt" className="headerTextDark">Alt Text Validator</h2>
-			<p className="bodyTextDark">Built to help content creators write best practive alt text.</p>
+			<p className="bodyTextDark">I built this application to help web content editors and social media managers at the Air and Space Museum write alt text that follows best practices.  The application can be expanded to include new vocabularies and logic checks, like checking if any color names are used, or if directional language is used.</p>
+			<p className="bodyTextDark">View the GitHub Repository: <a className="link" href="https://github.com/lapank/Alt-Text">https://github.com/lapank/Alt-Text</a></p>
 			</div>
 			<div className="container embed-responsive embed-responsive-4by3 altText">
 			<iframe title="Alt Text Checker" className="embed-responsive-item" src="https://kaylinlapan.com/alt-text/index.html" />
 			</div>
 			<div id="game" className="container pt-5">
 			<h2 className="headerTextDark">Tower Defense Game</h2>
-			<p className="bodyTextDark">Built with the HTML5 canvas API and JavaScript.</p>
+			<p className="bodyTextDark">Web browser game built with the HTML5 Canvas API.  I worked on this game with a team of 3 developers.  It includes 3 levels with various enemies and maps.  A save functionality and a chet code that unlocks all the levels.  Type “cheat” to access all levels.</p>
+			<p className="bodyTextDark">View the GitHub Repository: <a className="link" href="https://github.com/lapank/CS467-Tower-Defense">https://github.com/lapank/CS467-Tower-Defense</a></p>
 			</div>
 			<div className="container relative embed-responsive embed-responsive-4by3">
 			<div className="fullScreenWarning"><p className="smallTitleDark">View on a wider browser window to play.</p></div>
