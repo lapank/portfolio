62973486b0f6f5c3ca5554861602b4cc0a51b924
Kaylin Lapan <kaylinlapan@gmail.com>
2022-08-27 20:28:20 -0400

    Adjusted dates and added styling to Gantt chart.

--- src/components/charts/GanttChart.js
+++ src/components/charts/GanttChart.js
@@ -19,40 +19,39 @@ function GanttChart ({google}) {
       data.addColumn('string', 'Dependencies');
 
 
-    data.addRows([
-        ['2023Spring', 'Technical Inventory', 'Developers',
-         new Date(2023, 2, 6), new Date(2023, 3, 24), null, 100, null],
-        ['2023Summer', 'UX Research', 'UX, IA and Designers',
-         new Date(2023, 2, 6), new Date(2023, 2, 17), null, 100, null],
-        ['2023Autumn', 'Wireframing', 'UX and IA',
-         new Date(2023, 2, 13), new Date(2023, 2, 24), null, 100, null],
-        ['2023Winter', 'Review and Revise Wireframes', 'Designers, UX and IA and Stakeholders',
-         new Date(2023, 2, 20), new Date(2023, 3, 3), null, 100, null],
-        ['2024Spring', 'High Fidelity Mock-ups', 'Designers',
-         new Date(2023, 3, 3), new Date(2023, 3, 17), null, 50, null],
-        ['2024Summer', 'Review and Revise Mock-ups', 'Designers, Developers and Stakeholders',
-         new Date(2023, 3, 10), new Date(2023, 3, 24), null, 0, null],
-        ['2024Autumn', 'Sprint One', 'Developers',
-         new Date(2023, 4, 3), new Date(2023, 4, 28), null, 0, null],
-         ['2024Winter', 'Review Build', 'QA and Stakeholders',
-         new Date(2023, 5, 1), new Date(2023, 5, 5), null, 0, null],
-         ['2', 'Sprint Two', 'Developers',
-         new Date(2023, 5, 1), new Date(2023, 6, 2), null, 0, null],
-         ['Football', 'Review Build', 'QA and Stakeholders',
-         new Date(2023, 5, 29), new Date(2023, 6, 2), null, 100, null],
-         ['3', 'Sprint Three', 'Developers',
-         new Date(2023, 6, 2), new Date(2023, 6, 30), null, 0, null],
-        ['Baseball', 'Accessibility Testing and Remediation', 'QA and Developers',
-         new Date(2023, 6, 2), new Date(2023, 6, 30), null, 14, null],
-        ['Basketball', 'Security Testing and Remediation', 'QA and Developers',
-         new Date(2023, 6, 2), new Date(2023, 6, 30), null, 86, null],
-        ['Hockey', 'Acceptance Testing', 'Stakeholders',
-         new Date(2023, 7, 4), new Date(2023, 7, 15), null, 89, null],
-         ['3', 'Final Remediation and Hand-off', 'Developers',
-         new Date(2023, 7, 4), new Date(2023, 7, 29), null, 89, null],
-      ]);
 
-    console.log(data);
+    data.addRows([
+        ['1', 'Technical Inventory', 'Developers',
+         new Date(2023, 2, 6), new Date(2023, 3, 24), null, 100, null],
+        ['2', 'UX Research', 'UX, IA and Designers',
+         new Date(2023, 2, 6), new Date(2023, 2, 17), null, 100, null],
+        ['3', 'Wireframing', 'UX and IA',
+         new Date(2023, 2, 13), new Date(2023, 2, 24), null, 100, null],
+        ['4', 'Review and Revise Wireframes', 'Designers, UX and IA and Stakeholders',
+         new Date(2023, 2, 20), new Date(2023, 3, 3), null, 100, null],
+        ['5', 'High Fidelity Mock-ups', 'Designers',
+         new Date(2023, 3, 3), new Date(2023, 3, 17), null, 100, null],
+        ['6', 'Review and Revise Mock-ups', 'Designers, Developers and Stakeholders',
+         new Date(2023, 3, 10), new Date(2023, 3, 24), null, 100, null],
+        ['7', 'Sprint One', 'Developers',
+         new Date(2023, 3, 24), new Date(2023, 4, 24), null, 100, null],
+         ['8', 'Review', 'QA and Stakeholders',
+         new Date(2023, 4, 10), new Date(2023, 4, 24), null, 100, null],
+         ['2', 'Sprint Two', 'Developers',
+         new Date(2023, 4, 24), new Date(2023, 5, 24), null, 100, null],
+         ['9', 'Review', 'QA and Stakeholders',
+         new Date(2023, 5, 10), new Date(2023, 5, 24), null, 100, null],
+         ['10', 'Sprint Three', 'Developers',
+         new Date(2023, 5, 24), new Date(2023, 6, 24), null, 10, null],
+        ['11', 'Accessibility Testing and Remediation', 'QA and Developers',
+         new Date(2023, 5, 24), new Date(2023, 6, 24), null, 50, null],
+        ['12', 'Security Testing and Remediation', 'QA and Developers',
+         new Date(2023, 5, 24), new Date(2023, 6, 24), null, 28, null],
+        ['13', 'Acceptance Testing', 'Stakeholders',
+         new Date(2023, 6, 24), new Date(2023, 7, 7), null, 0, null],
+         ['14', 'Final Remediation and Hand-off', 'Developers',
+         new Date(2023, 6, 24), new Date(2023, 7, 24), null, 0, null],
+      ]);
 
       // Set chart options
       let options = {
@@ -61,14 +60,13 @@ function GanttChart ({google}) {
         gantt: {
           palette:[
             {
-              "color": "#f00",
-              "dark": "#0f0"
-            }
-            
+              "color": "#0b79ad",
+              "dark": "#45e7d8"
+            },
               
           ],
           trackHeight: 30,
-          innerGridTrack: {fill: '#fcfaf5;'},
+          innerGridTrack: {fill: '#fcfaf5'},
           innerGridDarkTrack: {fill: '#ede6da'},
           innerGridHorizLine:{stroke: '#fcfaf5'},
           percentStyle:{fill:'#82e6da'},
