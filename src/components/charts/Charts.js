e048198d670b21a7f7b947ce47f851ba11a1ed29
Kaylin Lapan <kaylinlapan@gmail.com>
2022-08-28 13:58:25 -0400

    Added content to Intranet case study.

--- src/components/charts/Charts.js
+++ src/components/charts/Charts.js
@@ -14,25 +14,49 @@ function Charts() {
   return (
     <div className="w-100 p-5">
     <h2 className="headerText gradientText">Case Study: Company Intranet</h2>
-    <p className="bodyTextLight">Sanky Tree, Line Chart, Gantt Chart, Pie Chart. Discovery Period. Take stock of intranet's current offerings, goals, and pain points before charting a path forward. How do People Currently use the Intranet?</p>
+    <p className="bodyTextLight">This Gantt chart outlines the project schedule for the site redesign and rebuild. The schedule emphasizes iterative development and design to ensure both the stakeholders and build team’s expectations stay inline.</p>
+
+    <div className='row'>
+          <GanttChart google={google} />
+          <div className="bodyTextLight">
+          <h3>Discovery Period</h3>
+          <p>A month long discovery phase will allow developers to examine the existing site to take an inventory of existing components, functionality, and 3rd party site integrations.  While this is occurring designers, and UX/IA specialists will conduct stakeholder interviews to determine aspects of the existing site users like, dislike, and functionality users would like to add.</p>
+          <h3>Design Phase</h3>
+          <p>While developers are still examining the existing site as part of the Discovery Period, designers, and UX/IA specialists will move into the Design Phase and produce wireframes for stakeholders to review.  After a round of review and remediation with the stakeholders, the wireframes will be turned into high fidelity mock-ups by the designers.  These mock-ups will then be reviewed by the stakeholders and the developers to make sure the mock-ups are not only designs the stakeholders approve, but also designs that the developers can build within the allotted timeframe.  If too many component variations are present this is the time the developers will notify the designers and the designers will produce a simplified design to show the stakeholders.  If a simplified design is needed, and the stakeholders do not approve the simplified design, the projects scope will need to be reassessed.  This review period is also when the site design will be run through its first accessibility tests.</p>
+          <h3>Build Phase</h3>
+          <p>Once the design of the site’s IA and components are settled, the developers will begin the build phase.  The build phase is divided into 3 sprints that each last a month.  Towards the end of each sprint, stakeholders will be shown site demos to ensure the build matches their expectations.  The last sprint will include accessibility and security testing to ensure the site meets required accessibility and security standards and will pass the organizations Technical Review Board.</p>
+          <h3>Review and Acceptance</h3>
+          <p>The last month of the project will consist of final reviews by the stakeholders and QA teams and final fixes and remediations from the developers to resolve any outstanding issues.</p>
+          </div>
+        </div>
+        <div className="bodyTextLight">
+        <h2>Discovery Period In-Depth</h2>
+        </div>
+      <p className="bodyTextLight">During the Discovery Phase, the UX and IA team will create Customer Journey maps and dive into the website’s existing analytics.  The following charts are a sample of what they will find to help inform their stakeholder conversations and wireframe development.</p>
     	<div className='row'>
     		<div className="col-6">
         <div className="lightCard">
-        <p className="bodyTextDark">This date was gathered from the Feedback Survey and Suggestion box on Skynet to create a total of 122 responses from 2017 to present.</p>
         <div className="centerAbsolute">
           <PizzaChart google={google}/>
         </div>
         </div>
+        <div className="bodyTextLight">
+        <h3>Intranet’s Use for Communication</h3>
+        <p>This pie chart shows data gathered from the Feedback Survey and Suggestion box on the intranet site totaling 122 responses from 2017 to present.  From this data the UX and IA team can gather that the intranet is seen as a way to communicate personnel and facility needs at the organization.  Follow-up questions include understanding if this workflow is effective in getting requests to the individuals who need to see them.  It would also be helpful to know what other tools and method’s staff use to communicate their organizational needs and how effective are those methods.</p>
   	    </div>
+        </div>
   	    <div className="col-6">
         <div className="lightCard">
         <div className="centerAbsolute">
           <LineChart google={google} />
         </div>
         </div>
+        <div className="bodyTextLight">
+        <h3>Intranet’s Visitation</h3>
+        <p>This line chart shows a downward trend with visitors using the organizations intranet.  This data is good for stakeholder interviews where UX and IA experts can uncover why site usage may have dropped.  It’s possible the drop in usage correlates to a lack of upkeep concerning site content and site functionality.  There may also be larger organizational changes and shifts or new workflows that effect how useful the intranet is.  At which point, it is the UX and IA experts job to discover out what would make the intranet useful to stakeholders again.</p>
   			</div>
+        </div>
       </div>
-        
         <div className='row mt-5 mb-5'>
           <div className="col-4">
           <div className="lightCard">
@@ -58,29 +82,12 @@ function Charts() {
             </div>
             </div>
           </div>
+          <div className="bodyTextLight">
+          <h3>Site Navigation</h3>
+          <p>These Sanky Tree charts show how user’s navigation through the intranet changed from 2017 to 2019.  Mostly site usage has stayed the same, there is a high number of drop-offs which might indicate a user need that is missing from the site, and many users continue to navigate towards resource pages, organizational charts, and other administrative pages.  In 2017 a staff café page was popular on the site, and it disappeared in following years.  It would be important to establish what happened to that section of the site and if that resource is still needed.</p>
+          </div>
         </div>
-        <div className='row'>
-        <p className="bodyTextLight">We spend 3 weeks taking stock of existing content types, components, external site integrations, and custom functionality. (Discovery phase)
-  
-Simultaneously we talk to staff about their wish list for the site, things they like and things they wish they could do with Skynet. 2 weeks (Discovery phase)
-
-1 week to map out a new information architecture and general page functionality and run this by team(Discovery)
-
-1 week take stackholder feedback and retool ai (discovery)
-
-2 weeks – develop look and feel for website with stakeholder interviews and component mock-ups
-
-3 weeks – develop wireframes for site
-
-1 week – examine the number of components being created and reused and ensure this is doable by the dev team
-
-2 weeks – develop high fidelity mockups for wireframes
-
-1 week review with stakholders
-
-3 months – development and review accessibility testing and security scanning TRB approval</p>
-          <GanttChart google={google} />
-        </div>
+        
         <h2 className="headerText">Customer Journey Map:</h2>
         <table className="table">
         <thead>
@@ -123,6 +130,10 @@ Simultaneously we talk to staff about their wish list for the site, things they 
           </tr>
         </tbody>
       </table>
+      <div className="bodyTextLight">
+          <h3>Users Deciding to Adopt the Site</h3>
+          <p>This customer journey map will be developed during the stakeholder interviews to better understand the Intranet’s users and the Intranet’s drop off in user retention.  In partnership with personas and other UX tools, these models will help design a sight that takes into consideration a user’s specific needs and behaviors.</p>
+          </div>
 		</div>
   );
 }
