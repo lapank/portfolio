bbd5e9868a61d6da1a6e5e950990ace5e0393b28
Kaylin Lapan <kaylinlapan@gmail.com>
2022-11-09 22:37:56 -0500

    Restyled homepage header.

--- src/components/ThreeCol.js
+++ src/components/ThreeCol.js
@@ -1,36 +1,32 @@
+import './ThreeCol.scss';
 
 function ThreeColBoxContent(props) {
   // Correct! There is no need to specify the key here:
   return (
-    <div style={{backgroundImage: `url("${props.image}")`, backgroundRepeat: 'no-repeat', backgroundSize: 1700, backgroundPosition: 'center', height:800}}>
-    <div className='row'>
-    <div className="col-md-6">
-        </div>
-       <div className="col-md-6 p-5">
-         <div className='row'>
-           <div className="col-md-12">
-           <div className="p-3">
-            <h3 className="headerTextDark large">{props.h3}</h3>
-            </div>
-           </div>
-         </div>
-        <div className='row'>
-          <div className="col-md-4">
-            <div className="p-3">
-                <h4 className="accentTextDark">{props.eyebrow}</h4>
-            </div>
-           </div>
-           <div className="col-md-8">
-            <div className="p-3">
-              <p className="bodyTextDark">{props.text}</p>
-            </div>
-           </div>
-        </div>
-       </div>
+    <div>
+    <div className="absoluteContainer">
+    <img className="threeColBackgroundImage" src={props.image} />
+    <div className="row topText">
+      <div className="col-5">
       </div>
-    		
-
-  	</div>	
+      <div className="col-7">
+        <div className="row">
+          <div className="col-12">
+            <h3 className="threeColHeader large">{props.h3}</h3>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-3">
+            <h4 className="threeColAccent">{props.eyebrow}</h4>
+          </div>
+          <div className="col-md-9">
+            <p className="threeColText">{props.text}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    </div>
+    </div>  
   );
 }
 
@@ -45,7 +41,7 @@ function ThreeColBox(props) {
       eyebrow = {item.eyebrow}
       h3 = {item.h3}
       text = {item.text} />
-	);
+  );
   return (
     <div className="w-100">
       {listItems}
