6b90bd42fb33dee47994274c1b8daea806c210d9
Kaylin Lapan <kaylinlapan@gmail.com>
2023-01-16 11:47:48 -0500

    Link component needed to be utilized instead of anchor tags.  Anchor tags trigger the application to attempt getting files from the server.

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Home from './pages/home.js';
+import Development from './pages/development.js';
+import Art from './pages/art.js';
+import Management from './pages/management.js';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+function App(){
+	
+	return(
+			<div className='App' style={{overflow: 'hidden'}}>
+			<Router>
+				<Routes>
+					<Route path='/' exact element={<Home />}/>
+					<Route path='/web-development' element={<Development />}/>
+					<Route path='/art-digital-media' element={<Art />}/>
+					<Route path='/product-owner-project-management' element={<Management />}/>
+				</Routes>
+			</Router>
+			</div>
+		);
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,42 +1,13 @@
-import Home from './pages/home';
-import Development from './pages/development';
-import Art from './pages/art';
-import Management from './pages/management';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import Error from "./pages/error";
-import React from "react";
-import ReactDOM from "react-dom/client";
-import {
-  createHashRouter,
-  RouterProvider,
-} from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+import App from './App';
+import Head from './Head';
 
-
-const router = createHashRouter([
-	{
-	  path: "/",
-	  element: <Home />,
-	},
-	{
-		errorElement: < Error/>
-	},
-	{
-		path: "/web-development",
-		element:< Development/>
-	},
-	{
-		path: "/art-digital-media",
-		element: <Art/>
-	},
-	{
-		path: "/product-owner-project-manager",
-		element: <Management/>
-	},
-  ]);
-
-  
-ReactDOM.createRoot(document.getElementById("root")).render(
-<React.StrictMode>
-	<RouterProvider router={router} />
-</React.StrictMode>
-);
+ReactDOM.render(
+  <React.StrictMode>
+    <Head />
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+  )
