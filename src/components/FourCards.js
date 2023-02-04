2223376756c00abd7b9ce1a00ab5937ff7dee50a
Kaylin Lapan <kaylinlapan@gmail.com>
2023-02-04 10:51:30 -0500

    Resize header and button placement on 4 col cards for mobile.

--- src/components/FourCards.js
+++ src/components/FourCards.js
@@ -13,7 +13,7 @@ function FourCardsContent(props) {
         <div className="card-body">
           <h5 className="card-title ">{props.title}</h5>
           <p className="card-text">{props.text}</p>
-          <a href={props.link} className="btn btn-primary fourColButton">{props.button}</a>
+          <div className="btn btn-primary fourColButton">{props.button}</div>
           </div>
         </div></a>
     </div>
